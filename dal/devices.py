d18cd81599ffee5c8e2aebacfebf2c6ba1049f7f
Shahar Band <shahar1531@gmail.com>
2024-01-30 12:03:43 +0200

    database config abstraction

--- dal/devices.py
+++ dal/devices.py
@@ -1,6 +1,7 @@
 from pydal import DAL, Field
+from utils.config import DB_URI
 
-db = DAL('postgres://postgres:123456@localhost:5432/sentinel')
+db = DAL(DB_URI)
 
 db.define_table('devices',
                 Field('name', 'string', length=128, required=True),
--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,9 @@
 VERSION = "0.0.1"
+
+# Database Configuration:
+DB_USERNAME = "postgres"
+DB_PASSWORD = "123456"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "sentinel"
+DB_URI = f'postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
