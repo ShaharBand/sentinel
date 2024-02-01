1b50d8b9ae1e6250132a12af1b59f392291a2170
Shahar Band <shahar1531@gmail.com>
2024-02-01 16:13:52 +0200

    front end redesign

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,31 +1,35 @@
 import logo from "../../assets/images/logo.png";
 
-import devices_icon from "../../assets/images/icons/timed-document.svg";
-import dashboard_icon from "../../assets/images/icons/cloud-upload.svg";
-import agents_icon from "../../assets/images/icons/adjustment.svg";
+import devices_icon from "../../assets/images/icons/dashboard.svg";
+import agents_icon from "../../assets/images/icons/pepole.svg";
+import bulk_actions_icon from "../../assets/images/icons/layers.svg";
+import settings_icon from "../../assets/images/icons/settings.svg";
+import logout_icon from "../../assets/images/icons/logout.svg";
 
 import "./style.css";
 
 const NavigationBar = () => (
   <nav className="navbar-container">
-    <a href="/">
-      <img
-        src={devices_icon}
-        alt="Devices Scan Dashboard"
-        className="icon-img"
-      />
-    </a>
-    <a href="/">
-      <img src={dashboard_icon} alt="General Dashboard" className="icon-img" />
-    </a>
-    <a href="/">
-      <img src={agents_icon} alt="Agents Dashboard" className="icon-img" />
-    </a>
-
     <div className="logo-container">
       <img src={logo} alt="Logo" className="logo-img" />
       <div className="logo-label">Sentinel</div>
     </div>
+
+    <a href="/">
+      <img src={devices_icon} alt="Devices Dashboard" className="icon-img" />
+    </a>
+    <a href="/">
+      <img src={agents_icon} alt="Agents Dashboard" className="icon-img" />
+    </a>
+    <a href="/">
+      <img src={bulk_actions_icon} alt="Bulk Actions" className="icon-img" />
+    </a>
+    <a href="/">
+      <img src={settings_icon} alt="Settings" className="icon-img" />
+    </a>
+    <a href="/">
+      <img src={logout_icon} alt="Logout" className="icon-img" />
+    </a>
   </nav>
 );
 export default NavigationBar;
--- frontend/src/views/DevicesPage/DevicesPage.tsx
+++ frontend/src/views/DevicesPage/DevicesPage.tsx
@@ -5,13 +5,16 @@ const DevicesPage = () => {
   return (
     <div className="page-container">
       <NavigationBar />
-      <div className="content">
-        <DeviceCard
-          name="Device Name"
-          tags={["tag1", "tag2", "tag3"]}
-          agents={["agent1", "agent2", "agent3"]}
-          address="88.88.88.88"
-        />
+      <div className="content-container">
+        <div className="content">
+          <h2>Overview</h2>
+          <DeviceCard
+            name="Device Name"
+            tags={["tag1", "tag2", "tag3"]}
+            agents={["agent1", "agent2", "agent3"]}
+            address="88.88.88.88"
+          />
+        </div>
       </div>
     </div>
   );
