62cbb566105923393133d5178a2d7ae47c599274
Shahar Band <shahar1531@gmail.com>
2024-02-06 11:05:59 +0200

    device service.py improvment

--- backend/src/services/devices.py
+++ backend/src/services/devices.py
@@ -2,6 +2,8 @@ from src.dal.devices import db
 from src.models.device import Device
 from src.models.devices.windows import WindowsDevice
 
+# TODO: device_factory.py to be more open closed principle rather than each time telling device type based on OS.
+
 
 def find_device_by_name(name: str, os_type: str) -> Device | None:
     query = (db.devices.name == name) & (db.devices.os_type == os_type)
@@ -14,5 +16,19 @@ def find_device_by_name(name: str, os_type: str) -> Device | None:
 
 
 def get_all_devices() -> list[Device] | None:
-    # TODO: Add logic to retrieve all devices
-    pass
+    devices = []
+    query = db.devices.id > 0
+    device_records = db(query).select()
+
+    for device_record in device_records:
+        if "windows" in device_record.os_type.lower():
+            devices.append(WindowsDevice(**device_record))
+        # elif "linux" in device_record.os_type.lower():
+        #     devices.append(LinuxDevice(**device_record))
+        # elif "mac" in device_record.os_type.lower():
+        #     devices.append(MacDevice(**device_record))
+
+    if devices:
+        return devices
+    return None
+
