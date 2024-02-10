dab4c4ef7e723bbdc4c99d63eeebf594321fee1e
Shahar Band <shahar1531@gmail.com>
2024-02-10 16:53:03 +0200

    services code quality improvment

--- backend/src/services/devices.py
+++ backend/src/services/devices.py
@@ -9,7 +9,7 @@ def find_device_by_id(device_id: int) -> Device | None:
     return DeviceFactory.create_device({**device_record}) if device_record else None
 
 
-def find_device_by_name(name: str) -> list[Device] | None:
+def find_device_by_name(name: str) -> list[Device]:
     query = (db.devices.name == name)
     device_records = db(query).select()
 
@@ -17,10 +17,10 @@ def find_device_by_name(name: str) -> list[Device] | None:
     for device_record in device_records:
         devices.append(DeviceFactory.create_device({**device_record}))
 
-    return devices if devices else None
+    return devices
 
 
-def get_all_devices() -> list[Device] | None:
+def get_all_devices() -> list[Device]:
     query = db.devices.id > 0
     device_records = db(query).select()
 
@@ -28,4 +28,4 @@ def get_all_devices() -> list[Device] | None:
     for device_record in device_records:
         devices.append(DeviceFactory.create_device({**device_record}))
 
-    return devices if devices else None
+    return devices
