3827b9831a5790a7beabbb3b1c1f67babe79da3a
Shahar Band <shahar1531@gmail.com>
2024-02-10 16:50:39 +0200

    device factory devices map dict

--- backend/src/models/devices/device_factory.py
+++ backend/src/models/devices/device_factory.py
@@ -1,16 +1,15 @@
 from src.models.devices.device import Device
 from src.models.devices.windows_device import WindowsDevice
 
+DEVICE_CLASS_MAP = {
+    "windows": WindowsDevice,
+    # "linux": LinuxDevice,
+}
+
 
 class DeviceFactory:
     @staticmethod
     def create_device(device_record: dict) -> Device | None:
-        os_type = device_record.get('os_type', '').lower()
-        if "windows" in os_type:
-            return WindowsDevice(**device_record)
-        # TODO: implement more types of devices in the future:
-        #       elif "linux" in os_type:
-        #           return LinuxDevice(**device_record)
-        #       elif "mac" in os_type:
-        #           return MacDevice(**device_record)
-        return None
+        os_type = device_record.get("os_type", "").lower()
+        device_class = DEVICE_CLASS_MAP.get(os_type)
+        return device_class(**device_record) if device_class else None
