46d3ea6ff9d6cd635bd016bf5760194dcc83f0a6
Shahar Band <shahar1531@gmail.com>
2024-03-27 17:14:41 +0200

    (code quality): better name for config variable

--- backend/src/config/manager.py
+++ backend/src/config/manager.py
@@ -4,6 +4,11 @@ from src.config.app import AppConfig
 from src.config.db import IntegrationDBConfig, ProductionDBConfig, DBConfig
 from src.config.environment import Environment
 
+ENVIRONMENT_TO_DB_CONFIG = {
+    Environment.integration: IntegrationDBConfig(),
+    Environment.production: ProductionDBConfig(),
+}
+
 
 class ConfigManager:
     @classmethod
@@ -14,8 +19,4 @@ class ConfigManager:
     @classmethod
     @lru_cache(maxsize=1)
     def get_db_config(cls) -> DBConfig:
-        db_config_map = {
-            Environment.integration: IntegrationDBConfig(),
-            Environment.production: ProductionDBConfig(),
-        }
-        return db_config_map.get(cls.get_app_config().ENVIRONMENT, IntegrationDBConfig())
+        return ENVIRONMENT_TO_DB_CONFIG.get(cls.get_app_config().ENVIRONMENT)
